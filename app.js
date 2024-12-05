cf449aae403f6b504d454accb28f04780d563e44
DaffaHavez <daffa.havez00@gmail.com>
2024-12-05 20:09:08 +0400

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.set('port',3000)
 app.use((req,res,next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://kogaweidner.github.io'); // Allow only specific origin
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
