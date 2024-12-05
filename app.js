05d8c1e309a34fdd9e1cb992bd4662eed786e011
DaffaHavez <daffa.havez00@gmail.com>
2024-12-05 18:52:26 +0400

    update

--- app.js
+++ app.js
@@ -27,62 +27,15 @@ MongoClient.connect('mongodb+srv://sokolig:Wednesday@cluster0.n4bti.mongodb.net/
   console.log('Connected to MongoDB');
 });
 
-// Root path message
-app.get('/', (req, res) => {
-  res.send("Select a collection, e.g., /collection/messages");
-});
-
-// Middleware to get the collection
-app.param("collectionName", (req, res, next, collectionName) => {
-  req.collection = db.collection(collectionName);
-  next();
-});
-
-// Retrieve all objects from collection
-app.get('/collection/:collectionName', (req, res, next) => {
+// API routes
+app.get('/api/collections/:collectionName', (req, res) => {
   req.collection.find({}).toArray((err, results) => {
-    if (err) return next(err);
+    if (err) return res.status(500).send(err);
     res.send(results);
   });
 });
 
-// Insert a new object into the collection
-app.post('/collection/:collectionName', (req, res, next) => {
-  req.collection.insertOne(req.body, (err, result) => {
-    if (err) return next(err);
-    res.send(result);
-  });
-});
-
-// Retrieve a single object by ID
-app.get('/collection/:collectionName/:id', (req, res, next) => {
-  req.collection.findOne({ _id: new ObjectID(req.params.id) }, (err, result) => {
-    if (err) return next(err);
-    res.send(result);
-  });
-});
-
-// Update an object by ID
-app.put('/collection/:collectionName/:id', (req, res, next) => {
-  req.collection.updateOne(
-    { _id: new ObjectID(req.params.id) },
-    { $set: req.body },
-    (err, result) => {
-      if (err) return next(err);
-      res.send({ msg: 'success' });
-    }
-  );
-});
-
-// Delete an object by ID
-app.delete('/collection/:collectionName/:id', (req, res, next) => {
-  req.collection.deleteOne({ _id: new ObjectID(req.params.id) }, (err, result) => {
-    if (err) return next(err);
-    res.send((result.deletedCount === 1) ? { msg: 'success' } : { msg: 'error' });
-  });
-});
-
-// Serve frontend static files
+// Serve static files from the frontend
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Route to serve frontend (index.html) for any unrecognized routes
@@ -90,7 +43,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
