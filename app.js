a8871b11bed54b6cbd560f92c3fb585659bb6a41
DaffaHavez <daffa.havez00@gmail.com>
2024-12-06 13:55:38 +0400

    removed logs

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-    console.log('[${new Date().toISOString()}] ${req.method} ${req.url}');
-})
 //configuring Express.js
 app.use(express.json())
 app.set('port',3000)
